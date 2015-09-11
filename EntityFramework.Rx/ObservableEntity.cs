8f14f01fabce92627eba3376e0b76e8ce7b5ef67
Nick Strupat <nick.strupat@gmail.com>
2015-09-11 09:00:03 -0400

    Initial code commit

---
+++ EntityFramework.Rx/ObservableEntity.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Data.Entity;
+using System.Reactive.Linq;
+
+using EntityFramework.Triggers;
+
+namespace EntityFramework.Rx {
+	public static class ObservableEntity {
+		private static Func<DbContext> dbContextFactory;
+		public static void RegisterDbContextFactory<TDbContext>(Func<TDbContext> factory) where TDbContext : DbContext => dbContextFactory = factory;
+
+		public static IObservable<IBeforeEntry<TEntity>> FromInserting<TEntity>() where TEntity : class, ITriggerable {
+			return Observable.FromEvent<IBeforeEntry<TEntity>>(action => Triggers<TEntity>.Inserting += entry => {
+				using (var context = dbContextFactory())
+					action(new Entries<TEntity>.InsertingEntry(entry, context));
+			}, action => { });
+		}
+		public static IObservable<IBeforeEntry<TEntity>> FromUpdating<TEntity>() where TEntity : class, ITriggerable {
+			return Observable.FromEvent<IBeforeEntry<TEntity>>(action => Triggers<TEntity>.Updating += action, action => Triggers<TEntity>.Updating -= action);
+		}
+		public static IObservable<IBeforeEntry<TEntity>> FromDeleting<TEntity>() where TEntity : class, ITriggerable {
+			return Observable.FromEvent<IBeforeEntry<TEntity>>(action => Triggers<TEntity>.Deleting += action, action => Triggers<TEntity>.Deleting -= action);
+		}
+		public static IObservable<IFailedEntry<TEntity>> FromInsertFailed<TEntity>() where TEntity : class, ITriggerable {
+			return Observable.FromEvent<IFailedEntry<TEntity>>(action => Triggers<TEntity>.InsertFailed += action, action => Triggers<TEntity>.InsertFailed -= action);
+		}
+		public static IObservable<IFailedEntry<TEntity>> FromUpdateFailed<TEntity>() where TEntity : class, ITriggerable {
+			return Observable.FromEvent<IFailedEntry<TEntity>>(action => Triggers<TEntity>.UpdateFailed += action, action => Triggers<TEntity>.UpdateFailed -= action);
+		}
+		public static IObservable<IFailedEntry<TEntity>> FromDeleteFailed<TEntity>() where TEntity : class, ITriggerable {
+			return Observable.FromEvent<IFailedEntry<TEntity>>(action => Triggers<TEntity>.DeleteFailed += action, action => Triggers<TEntity>.DeleteFailed -= action);
+		}
+		public static IObservable<IEntry<TEntity>> FromInserted<TEntity>() where TEntity : class, ITriggerable {
+			return Observable.FromEvent<IEntry<TEntity>>(action => Triggers<TEntity>.Inserted += action, action => Triggers<TEntity>.Inserted -= action);
+		}
+		public static IObservable<IEntry<TEntity>> FromUpdated<TEntity>() where TEntity : class, ITriggerable {
+			return Observable.FromEvent<IEntry<TEntity>>(action => Triggers<TEntity>.Updated += action, action => Triggers<TEntity>.Updated -= action);
+		}
+		public static IObservable<IEntry<TEntity>> FromDeleted<TEntity>() where TEntity : class, ITriggerable {
+			return Observable.FromEvent<IEntry<TEntity>>(action => Triggers<TEntity>.Deleted += action, action => Triggers<TEntity>.Deleted -= action);
+		}
+	}
+}
---
+++ Example/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading;
+
+using EntityFramework.Rx;
+using EntityFramework.Triggers;
+
+namespace Example {
+	class Program {
+		static void Main(String[] args) {
+			ObservableEntity.RegisterDbContextFactory(() => new Context());
+			var observerThread = new Thread(ObserveNewPeople);
+			observerThread.Start();
+			Thread.Sleep(100);
+			using (var context = new Context()) {
+				Console.WriteLine("context");
+				context.People.Add(new Person { Name = "Nick", DateOfBirth = new DateTime(1986, 7, 17) });
+				context.SaveChanges();
+				context.People.Add(new Person { Name = "Joe", DateOfBirth = new DateTime(1986, 9, 11) });
+				context.SaveChanges();
+				Console.WriteLine("saved");
+			}
+			observerThread.Join();
+		}
+
+		private static void ObserveNewPeople() {
+			Console.WriteLine("thread");
+			var o = ObservableEntity.FromInserting<Person>();
+			using (var p = o.Where(x => x.Entity.DateOfBirth.Month == DateTime.Today.Month && x.Entity.DateOfBirth.Day == DateTime.Today.Day).Subscribe(entry => Console.WriteLine($"Happy birthday to {entry.Entity.Name}!"))) {
+				Thread.Sleep(100000);
+			}
+		}
+	}
+
+	public class Context : DbContextWithTriggers {
+		public virtual DbSet<Person> People { get; set; }
+	}
+
+	public class Person : ITriggerable {
+		public Int64 Id { get; private set; }
+		public String Name { get; set; }
+		[DataType(DataType.Date)]
+		public DateTime DateOfBirth { get; set; }
+	}
+}

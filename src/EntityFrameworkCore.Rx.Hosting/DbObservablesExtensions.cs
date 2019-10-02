731e439cd885bf203115cd16a73975b662cf1ed7
NickStrupat <nick.strupat@gmail.com>
2019-10-01 23:29:57 -0400

    create hosting projects and fix ASP.NET Core projects

---
+++ src/EntityFrameworkCore.Rx.Hosting/DbObservablesExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.Extensions.Hosting;
+#if EF_CORE
+namespace EntityFrameworkCore.Rx.Hosting
+#else
+namespace EntityFramework.Rx.Hosting
+#endif
+{
+	public static class DbObservablesExtensions
+	{
+		public static IHost UseDbObservables(this IHost host, Action<IDbObservablesBuilder> configureDbObservables)
+		{
+			if (configureDbObservables == null)
+				throw new ArgumentNullException(nameof(configureDbObservables));
+			var dbObservablesBuilder = DbObservablesBuilderFactory.Create(host.Services);
+			configureDbObservables.Invoke(dbObservablesBuilder);
+			return host;
+		}
+	}
+}
